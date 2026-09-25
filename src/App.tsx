fbf7a101371a4901968145a03b6654f4f6bb4d44
Shaniza <shaniza.rr17@gmail.com>
2026-09-25 10:31:31 +0600

    Banner section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Nav from "./components/Nav";
+import Banner from "./components/Banner";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Nav />
+      <Banner />
 
     </div>
   );
